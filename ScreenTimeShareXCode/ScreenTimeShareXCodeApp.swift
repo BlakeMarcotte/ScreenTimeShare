b4737c24d8f46fcb0269e8a8eebd81a240eac74f
BlakeMarcotte <blakemarcotte@gmail.com>
2025-10-21 23:04:27 -0400

    Initial Commit

---
+++ ScreenTimeShareXCode/ScreenTimeShareXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeShareXCodeApp.swift
+//  ScreenTimeShareXCode
+//
+//  Created by Blake Marcotte on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeShareXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
